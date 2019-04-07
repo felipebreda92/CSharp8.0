5f410341f57f43600169b66d9f4cc5d52ae78d06
felipebreda92 <felipebreda92@gmail.com>
2019-04-07 11:59:54 -0300

    Add Project UsingDeclarations with Marvel API requests

---
+++ UsingDeclarations/Program.cs
@@ -0,0 +1,189 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UsingDeclarations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using var md5 = MD5.Create();
+                var hash = GetMd5Hash(md5, "1" + "d4205f94d67bd6deb40f9b5490196e2673aa4da7" + "38559f0dd9819d43e0ea5ad015a932d9");
+
+                var url = "http://gateway.marvel.com/v1/public/comics?";
+                url += "ts=1";
+                url += "&apikey=38559f0dd9819d43e0ea5ad015a932d9";
+                url += $"&hash={hash}";
+
+                var request = WebRequest.Create(url);
+
+                using var response = (HttpWebResponse)request.GetResponse();
+
+                Console.WriteLine(response.StatusDescription);
+
+                using Stream dataStream = response.GetResponseStream();
+                using StreamReader reader = new StreamReader(dataStream);
+
+                //var serverResponse = JsonConvert.DeserializeObject<RootObject>(reader.ReadToEnd());
+                var serverResponse = reader.ReadToEnd();
+
+                Console.WriteLine(serverResponse);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message + ":" + e.InnerException);
+            }
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+    }
+
+
+    public class Url
+    {
+        public string type { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Series
+    {
+        public string resourceURI { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Date
+    {
+        public string type { get; set; }
+        public object date { get; set; }
+    }
+
+    public class Price
+    {
+        public string type { get; set; }
+        public double price { get; set; }
+    }
+
+    public class Thumbnail
+    {
+        public string path { get; set; }
+        public string extension { get; set; }
+    }
+
+    public class Creators
+    {
+        public int available { get; set; }
+        public string collectionURI { get; set; }
+        public List<object> items { get; set; }
+        public int returned { get; set; }
+    }
+
+    public class Characters
+    {
+        public int available { get; set; }
+        public string collectionURI { get; set; }
+        public List<object> items { get; set; }
+        public int returned { get; set; }
+    }
+
+    public class Item
+    {
+        public string resourceURI { get; set; }
+        public string name { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Stories
+    {
+        public int available { get; set; }
+        public string collectionURI { get; set; }
+        public List<Item> items { get; set; }
+        public int returned { get; set; }
+    }
+
+    public class Events
+    {
+        public int available { get; set; }
+        public string collectionURI { get; set; }
+        public List<object> items { get; set; }
+        public int returned { get; set; }
+    }
+
+    public class Result
+    {
+        public int id { get; set; }
+        public int digitalId { get; set; }
+        public string title { get; set; }
+        public int issueNumber { get; set; }
+        public string variantDescription { get; set; }
+        public string description { get; set; }
+        public object modified { get; set; }
+        public string isbn { get; set; }
+        public string upc { get; set; }
+        public string diamondCode { get; set; }
+        public string ean { get; set; }
+        public string issn { get; set; }
+        public string format { get; set; }
+        public int pageCount { get; set; }
+        public List<object> textObjects { get; set; }
+        public string resourceURI { get; set; }
+        public List<Url> urls { get; set; }
+        public Series series { get; set; }
+        public List<object> variants { get; set; }
+        public List<object> collections { get; set; }
+        public List<object> collectedIssues { get; set; }
+        public List<Date> dates { get; set; }
+        public List<Price> prices { get; set; }
+        public Thumbnail thumbnail { get; set; }
+        public List<object> images { get; set; }
+        public Creators creators { get; set; }
+        public Characters characters { get; set; }
+        public Stories stories { get; set; }
+        public Events events { get; set; }
+    }
+
+    public class Data
+    {
+        public int offset { get; set; }
+        public int limit { get; set; }
+        public int total { get; set; }
+        public int count { get; set; }
+        public List<Result> results { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int code { get; set; }
+        public string status { get; set; }
+        public string copyright { get; set; }
+        public string attributionText { get; set; }
+        public string attributionHTML { get; set; }
+        public string etag { get; set; }
+        public Data data { get; set; }
+    }
+}
